30907e6e64af2fb7388ae2825bda94f211cd2da1
Carlos Santos <carlos.santos74@outlook.com>
2021-06-15 21:06:48 -0300

    Checkpoint

--- posinstall.sh
+++ posinstall.sh
@@ -6,17 +6,9 @@ echo "---------------------Iniciando----------------------"
 
 echo "---------------------------Pegando Variáveis----------------------"
 
-PPA_PHP_MY_ADMIN= "ppa:phpmyadmin/ppa"
 PPA_LUTRIS="ppa:lutris-team/lutris"
-PPA_GRAPHICS_DRIVERS="ppa:graphics-drivers/ppa"
 PPA_OBS_STUDIO="ppa:obsproject/obs-studio"
-PPA_SIMPLE_BATTERY="ppa:slimbook/slimbook"
-PPA_OPEN_RAZER="ppa:openrazer/stable"
-PPA_POLYCHROMATIC=" ppa:polychromatic/stable"
 
-
-URL_WINE_KEY="https://dl.winehq.org/wine-builds/winehq.key"
-URL_PPA_WINE="https://dl.winehq.org/wine-builds/ubuntu/"
 URL_GOOGLE_CHROME="https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
 URL_CODE="https://az764295.vo.msecnd.net/stable/91899dcef7b8110878ea59626991a18c8a6a1b3e/code_1.47.3-1595520028_amd64.deb"
 URL_DBEAVEAR="https://download.dbeaver.com/community/7.3.1/dbeaver-ce_7.3.1_amd64.deb"
@@ -47,13 +39,7 @@ sudo apt update -y
 
 ## Adicionando repositórios de terceiros e suporte a Snap, Flatpak e extensão gnome##
 sudo add-apt-repository "$PPA_LUTRIS" -y
-sudo apt-add-repository "$PPA_GRAPHICS_DRIVERS" -y
 sudo apt-add-repository "$PPA_OBS_STUDIO" -y
-sudo apt-add-repository "$PPA_PHP_MY_ADMIN" -y
-sudo apt-add-repository "$PPA_SIMPLE_BATTERY"
-wget -nc "$URL_WINE_KEY"
-sudo apt-key add winehq.key
-sudo apt-add-repository "deb $URL_PPA_WINE bionic main"
 sudo apt install snapd -y 
 sudo apt install flatpak -y 
 sudo apt install chrome-gnome-shell -y 
@@ -74,7 +60,6 @@ mkdir "$DIRETORIO_DOWNLOADS"
 ## Download e instalaçao de programas externos ##
 
 wget -c "$URL_GOOGLE_CHROME"       -P "$DIRETORIO_DOWNLOADS"
-wget -c "$URL_CLIQ"       -P "$DIRETORIO_DOWNLOADS"
 wget -c "$URL_CODE"     -P "$DIRETORIO_DOWNLOADS"
 wget -c "$URL_DBEAVEAR" -P "$DIRETORIO_DOWNLOADS"
 wget -c "$URL_MICRO" -P "$DIRETORIO_DOWNLOADS"
@@ -89,10 +74,8 @@ sudo apt install --install-recommends winehq-stable wine-stable wine-stable-i386
 sudo snap install sublime-text --classic
 sudo snap install mailspring
 sudo snap install telegram-desktop
-sudo snap install datagrip --classic
 sudo snap install discord --classic 
 sudo snap install atom --classic 
-sudo snap install figma-linux
 
 ## ------------------------- Instalando programas do repositorio ----------------------------##
 sudo apt-get install gnome-sushi -y
@@ -157,22 +140,6 @@ echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable ma
 
 sudo apt-get install brave-browser -y
 
-
-# ----------------------------- TOUCHPAD ----------------------------- #
-
-sudo gpasswd -a $USER input
-sudo apt-get install xdotool wmctrl
-
-git clone https://github.com/bulletmark/libinput-gestures.git
-cd libinput-gestures
-sudo ./libinput-gestures-setup install
-
-libinput-gestures-setup start
-libinput-gestures-setup autostart
-
-git clone https://gitlab.com/cunidev/gestures
-cd gestures
-sudo python3 setup.py install
 
 
 
