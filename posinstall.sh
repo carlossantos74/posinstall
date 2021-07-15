62ee3b72791e448fd4e206c13a89d8721f1cb4a1
Carlos Santos <carlos.santos74@outlook.com>
2021-07-15 17:32:38 -0300

    Mudanças no script

--- posinstall.sh
+++ posinstall.sh
@@ -84,6 +84,7 @@ sudo apt-get install caffeine -y
 sudo apt-get install gitg -y
 sudo apt-get install timeshift -y
 sudo apt-get install diodon -y
+sudo apt-get install steam -y
 
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install -y nodejs
