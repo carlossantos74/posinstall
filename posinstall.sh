055ec2b7bed6d4159f736ae3c8d4d41498b9928d
Carlos Santos <carlos.santos74@outlook.com>
2021-07-15 16:39:45 -0300

    Mudanças no script

--- posinstall.sh
+++ posinstall.sh
@@ -1,26 +1,18 @@
 !/usr/bin/env bash
 
-echo "---------------------Iniciando----------------------"
-
-# ----------------------------- VARIÁVEIS ----------------------------- #
-
-echo "---------------------------Pegando Variáveis----------------------"
-
 PPA_LUTRIS="ppa:lutris-team/lutris"
 PPA_OBS_STUDIO="ppa:obsproject/obs-studio"
+PPA_TIMESHIFT="ppa:teejee2008/timeshift"
+PPA_DIODON="ppa:diodon-team/stable"
 
 URL_GOOGLE_CHROME="https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
 URL_CODE="https://az764295.vo.msecnd.net/stable/91899dcef7b8110878ea59626991a18c8a6a1b3e/code_1.47.3-1595520028_amd64.deb"
 URL_DBEAVEAR="https://download.dbeaver.com/community/7.3.1/dbeaver-ce_7.3.1_amd64.deb"
 URL_MICRO="https://github-production-release-asset-2e65be.s3.amazonaws.com/53632140/84bb8680-07fb-11eb-8971-a0397105dc9b?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20210112%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20210112T175215Z&X-Amz-Expires=300&X-Amz-Signature=36c2a9ce7bd5741621353681f914ce1f207199acb4088bb10ced58e0c8b03c2b&X-Amz-SignedHeaders=host&actor_id=0&key_id=0&repo_id=53632140&response-content-disposition=attachment%3B%20filename%3Dmicro-2.0.8-amd64.deb&response-content-type=application%2Foctet-stream"
 URL_GITKRAKEN="https://release.axocdn.com/linux/gitkraken-amd64.deb"
-
+URL_EDGE="https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-beta/microsoft-edge-beta_92.0.902.45-1_amd64.deb"
 
 DIRETORIO_DOWNLOADS="$HOME/Downloads/programas"
-
-# ---------------------------------------------------------------------- #
-
-echo "---------------------Preparando os requisitos para a Instalaçao----------------------"
 
 # ----------------------------- REQUISITOS ----------------------------- #
 ## Removendo travas eventuais do apt ##
@@ -45,39 +37,23 @@ sudo apt install flatpak -y
 sudo apt install chrome-gnome-shell -y 
 sudo add-apt-repository ppa:caffeine-developers/ppa
 
-# ---------------------------------------------------------------------- #
-
-echo "---------------------Iniciando Instalaçao----------------------"
-
-# ----------------------------- EXECUÇÃO ----------------------------- #
-## Atualizando o repositório depois da adição de novos repositórios ##
 sudo apt update -y
-
-## Criação do diretório de downloads ##
 
 mkdir "$DIRETORIO_DOWNLOADS"
 
-## Download e instalaçao de programas externos ##
-
-wget -c "$URL_GOOGLE_CHROME"       -P "$DIRETORIO_DOWNLOADS"
-wget -c "$URL_CODE"     -P "$DIRETORIO_DOWNLOADS"
+wget -c "$URL_GOOGLE_CHROME" -P "$DIRETORIO_DOWNLOADS"
+wget -c "$URL_CODE" -P "$DIRETORIO_DOWNLOADS"
 wget -c "$URL_DBEAVEAR" -P "$DIRETORIO_DOWNLOADS"
 wget -c "$URL_MICRO" -P "$DIRETORIO_DOWNLOADS"
 wget -c "$URL_GITKRAKEN" -P "$DIRETORIO_DOWNLOADS" 
 
-## ------------------- Instalando pacotes .deb baixados na sessão anterior ----------------- ##
 sudo dpkg -i $DIRETORIO_DOWNLOADS/*.deb
 
-sudo apt install --install-recommends winehq-stable wine-stable wine-stable-i386 wine-stable-amd64 -y
-
-## ----------------------------- Instalando pacotes Snap ------------------------------------##
 sudo snap install sublime-text --classic
 sudo snap install mailspring
 sudo snap install telegram-desktop
-sudo snap install discord --classic 
-sudo snap install atom --classic 
+sudo snap install discord --classic
 
-## ------------------------- Instalando programas do repositorio ----------------------------##
 sudo apt-get install gnome-sushi -y
 sudo apt-get install gnome-tweak-tool -y
 sudo apt-get install git -y 
@@ -105,48 +81,13 @@ sudo apt-get install virtualbox -y
 sudo apt-get install piper -y
 sudo apt-get install caffeine -y
 sudo apt-get install gitg -y
+sudo apt-get install timeshift -y
+sudo apt-get install diodon -y
 
-
-
-#------------------------------ Node JS -------------------------------------# 
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
-
-#----------------------------- spotify -------------------------------------# 
-curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add - 
-echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-
-sudo apt-get install spotify-client -y 
-
-#----------------------------- Insomnia -------------------------------------# 
-
-# Add to sources
-echo "deb https://dl.bintray.com/getinsomnia/Insomnia /" \
-    | sudo tee -a /etc/apt/sources.list.d/insomnia.list
-
-# Add public key used to verify code signature
-wget --quiet -O - https://insomnia.rest/keys/debian-public.key.asc \
-    | sudo apt-key add -
-
-# Refresh repository sources and install Insomnia
-sudo apt-get install insomnia -y
-
-#----------------------------- Brave Browser -------------------------------------# 
-
-curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
-
-echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-
-sudo apt-get install brave-browser -y
-
-
-
-
-# ----------------------------- PÓS-INSTALAÇÃO ----------------------------- #
-
 ## Finalização, atualização e limpeza##
-
 flatpak update -y
 sudo rm /var/cache/fontconfig/*
 sudo rm ~/.cache/fontconfig/*
@@ -155,6 +96,3 @@ sudo apt update && sudo apt dist-upgrade -y
 sudo apt autoclean -y
 sudo apt autoremove -y
 
-echo "---------------------FINALIZADO----------------------"
-
-# ---------------------------------------------------------------------- #
